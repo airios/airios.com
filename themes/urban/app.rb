bcb8d429b7205cd2d0764bd07621288f930e1ca2
Abel Rios <arios@yahoo-inc.com>
2011-04-14 22:23:40 -0500

    Updating app.rb to use scss and set up with sass

--- themes/urban/app.rb
+++ themes/urban/app.rb
@@ -9,11 +9,45 @@ module Nesta
     # Put your assets in themes/urban/public/urban.
     #
     # use Rack::Static, :urls => ["/urban"], :root => "themes/urban/public"
+    configure do
+            
+      Compass.configuration do |config|
+        config.project_path = File.dirname(__FILE__)
+        config.sass_dir = 'views'
+        config.environment = :production
+        config.relative_assets = true
+        config.http_path = "/"
+        config.http_images_path = "../images"
+        config.images_dir = "../images"
+      end
+
+      set :haml, { :format => :html5 }
+      set :sass, Compass.sass_engine_options
+    end
+
+    get '/css/:layout.css' do
+      content_type 'text/css', :charset => 'utf-8'
+      cache scss(params[:layout].to_sym)
+    end
 
     helpers do
       # Add new helpers here.
+      def set_common_variables
+        @menu_items = Nesta::Menu.for_path('/')
+        @site_title = Nesta::Config.title
+        @default_author = Nesta::Config.author
+        set_from_config(:title, :subtitle, :google_analytics_code)
+        @heading = @title
+      end
+      
+      def format_date(date)
+        date.strftime('%B %d, %Y')
+      end
+      
+      def format_month( month )
+        m = Date::MONTHNAMES[ month ]
+      end
+      
     end
-
-    # Add new routes here.
   end
 end
